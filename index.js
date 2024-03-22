24cb23988d84d7ff5a51ad8b23984e1937dcf7a8
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-03-22 23:56:01 +0600

    Add array sum calculation example to index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// const a1 = new Array();
+// console.log(a1);
+
+// const a2 = new Array(5);
+// console.log(a2);
+
+// const b1 = Array(5);
+// console.log(b1);
+
+const arr = [1, 2, 3, 4, 5];
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(sum);
