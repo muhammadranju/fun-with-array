25c3893cfefdf19578e7bf3dca70636997aa7d6d
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-03-24 00:04:43 +0600

    Refactor index.js to include updated array and scaffold for finding 2nd largest number

--- index.js
+++ index.js
@@ -7,12 +7,45 @@
 // const b1 = Array(5);
 // console.log(b1);
 
-const arr = [1, 2, 3, 4, 5];
+const arr = [11, 25, 34, 49, 53, 77, 99, 51];
 
-let sum = 0;
+// let sum = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-}
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// +console.log(sum);
 
-console.log(sum);
+// find largest number
+
+// let largestNumber = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > largestNumber) {
+//     largestNumber = arr[i];
+//   }
+// }
+// console.log(largestNumber);
+
+// find smallest number
+// let smallestNumber = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < smallestNumber) {
+//     smallestNumber = arr[i];
+//   }
+// }
+// console.log(smallestNumber);
+
+// find 2nd largest number
+let secondNumberLarges = arr[2];
+
+for (let i = 1; i < arr.length; i++) {}
+console.log(secondNumberLarges);
+
+// // find 2nd smallest number
+// let secondSmallestNumber = arr[1];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < secondSmallestNumber) {
+//     secondSmallestNumber = arr[i];
+//   }
+// }
+// console.log(secondSmallestNumber);
