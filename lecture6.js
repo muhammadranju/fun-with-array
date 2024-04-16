42330c8279cabe5ef2aa547711c6761a9d2c1876
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-04-17 00:24:58 +0600

    feat: push method adding  in numbers array 10

---
+++ lecture6.js
@@ -0,0 +1,16 @@
+/**
+ * This is a simple script that demonstrates the use of arrays in JavaScript.
+ *
+ * It initializes an array of numbers from 1 to 9 and adds the number 10 to the end of the array.
+ *
+ * The numbers array is then logged to the console.
+ */
+
+// Define the array of numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Add the number 10 to the end of the array
+numbers.push(10);
+
+// Log the updated array to the console
+console.log(numbers);
