af19751af3fadf40fd0c95532a3c193e6d8e238d
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-04-18 00:07:42 +0600

    feat: Removes all elements from the `numbers` array that are equal to the value of `toBeDeleted1

--- lecture6.js
+++ lecture6.js
@@ -7,10 +7,33 @@
  */
 
 // Define the array of numbers
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 // Add the number 10 to the end of the array
-numbers.push(10);
+// numbers.push(10);
 
 // Log the updated array to the console
+
+// remove first element
+// numbers.shift();
+
+// remove last element
+// numbers.pop();
+// numbers.length = numbers.length - 1;
+
+// const toBeDeleted = 5;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] === toBeDeleted) {
+//     numbers.splice(i, 1);
+//   }
+// }
+
+const toBeDeleted1 = 8;
+
+/**
+ * Removes all elements from the `numbers` array that are equal to the value of `toBeDeleted1`.
+ */
+numbers = numbers.filter((item) => item !== toBeDeleted1);
+
 console.log(numbers);
